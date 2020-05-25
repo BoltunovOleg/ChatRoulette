f572d0533910be5d8ca9afd2ee128709ef932f96
MeowExort <agit0.o@yandex.ru>
2020-05-26 01:17:55 +0300

    Время начала сессии

--- ChatRoulette.Repository/Properties/AssemblyInfo.cs
+++ ChatRoulette.Repository/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.20146")]
-[assembly: AssemblyFileVersion("1.0.8.20146")]
+[assembly: AssemblyVersion("1.0.9.20146")]
+[assembly: AssemblyFileVersion("1.0.9.20146")]
 
-[assembly: AssemblyInformationalVersion("1.0.8")]
+[assembly: AssemblyInformationalVersion("1.0.9")]
